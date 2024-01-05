00a5e10b8056c47183d5b867537404b04789caef
squadri-qt <info@quadtechnology.co.uk>
2024-01-05 11:41:22 +0000

    astro jsx integration, ui component, islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-teal-five.vercel.app/"
+  site: "https://astro-blog-teal-five.vercel.app/",
+  integrations: [preact()]
 });
