2629ef4ed47af07402f594b35804ef59dedd64ba
squadri-qt <info@quadtechnology.co.uk>
2024-01-08 16:24:34 +0000

    site url update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-teal-five.vercel.app",
+  site: {
+    url: "https://astro-blog-teal-five.vercel.app",
+  },
   integrations: [preact()]
 });
