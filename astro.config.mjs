6a6575eefdc513c5d952d42f06ba0d1d8db68dfe
squadri-qt <info@quadtechnology.co.uk>
2024-01-08 16:39:33 +0000

    remove site attribute from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 
 export default defineConfig({
-  site: "https://astro-blog-teal-five.vercel.app",
   integrations: [preact()]
 });
