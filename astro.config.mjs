38c1e446592ea1095493484bd6ac95733e9ec968
squadri-qt <info@quadtechnology.co.uk>
2024-01-08 16:21:54 +0000

    test without https in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-teal-five.vercel.app/",
+  site: "astro-blog-teal-five.vercel.app",
   integrations: [preact()]
 });
