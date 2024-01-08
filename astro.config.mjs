bfc4b85eafd4250dce28b0ffd27ebcf235a65b4a
squadri-qt <info@quadtechnology.co.uk>
2024-01-08 16:44:50 +0000

    reverting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 
 export default defineConfig({
+  site: "https://astro-blog-teal-five.vercel.app",
   integrations: [preact()]
 });
